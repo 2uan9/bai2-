e93b4282e7910ded47b5a90de827059b23e0106d
vucd <vucd@QUANG>
2025-02-23 21:19:44 +0700

    bai2

---
+++ app/src/main/java/com/example/bai2/Message.java
@@ -0,0 +1,30 @@
+package com.example.bai2;
+
+public class Message {
+    private Long id;
+    private String title, body;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
